b59b4c0aa7c480a901e33728a467799ad717c56c
Souvik Maity <souvik.maity200@gmail.com>
2020-05-18 18:44:51 +0530

    minor change

---
+++ media-net/js/base.js
@@ -0,0 +1,74 @@
+(() => {
+    var localKey = 'mdata',
+    notSynced = [],
+    bindEvent = (el, event, fn) => {
+		el.addEventListener ? el.addEventListener(event, fn, false) : el.attachEvent && el.attachEvent('on' + event, fn);
+	},
+	// unbindEvent = (el, event, fn) => {
+	// 	el.removeEventListener ? el.removeEventListener(event, fn) : el.detachEvent && el.detachEvent('on' + event, fn);
+    // },
+    body = (document.body || document.getElementsByTagName('body')[0]);
+
+    bindEvent(document.getElementById('div'), 'click', () => {
+        fetch(`${window.location.protocol}//${window.location.host}/media-net/image/pixel.gif?interaction=UserClick&client=ad_media&os_name=macos&x1=google&x2=email&x3=pdfconvert&landing_url=abcd1&_=${Math.random() * 1E16}`)
+        .then(response => response.blob())
+        .then(images => {
+            // outside = URL.createObjectURL(images);
+            console.log(images)
+        })
+
+        function reqListener () {
+            console.log(this.responseText);
+        }
+        
+        var oReq = new XMLHttpRequest();
+        oReq.addEventListener("load", reqListener);
+        oReq.open("GET", `${window.location.protocol}//${window.location.host}/media-net/image/pixel.gif?interaction=UserClick&client=ad_media&os_name=macos&x1=google&x2=email&x3=pdfconvert&landing_url=abcd1&_=${Math.random() * 1E16}`);
+        oReq.send();
+
+        var image = document.createElement('img');
+        image.setAttribute('src', `${window.location.protocol}//${window.location.host}/media-net/image/pixel.gif?interaction=UserClick&client=ad_media&os_name=macos&x1=google&x2=email&x3=pdfconvert&landing_url=abcd1&_=${Math.random() * 1E16}`);
+        body.appendChild(image);
+    });
+
+    if('serviceWorker' in navigator) {
+        bindEvent(window, 'load', () => {
+            // var worker = new Worker('/js/service-worker.js'),
+            //     mdata = localStorage.getItem(localKey); // Fetching all unsynced data
+
+            // worker.addEventListener('message', (e) => {
+            //     console.log('Worker said: ', e.data);
+            //     notSynced.push(e)
+            // }, false);
+
+            // if(mdata){
+            //     mdata = JSON.parse(mdata);
+            //     worker.postMessage({cmd: 'SYNC', data: mdata});
+            //     localStorage.setItem(localKey, JSON.stringify([]));
+            // }
+
+            navigator.serviceWorker.register('/media-net/service-worker.js', { scope: '/media-net/' })
+            .then((reg) => {
+                console.log(`Log: Registration succeeded. Scope is ${reg.scope}`);
+                if(reg.installing) {
+                    console.log('Service worker installing');
+                } else if(reg.waiting) {
+                    console.log('Service worker installed');
+                } else if(reg.active) {
+                    console.log('Service worker active');
+                }
+            }, () => {
+                console.log('Log: service worker registration failure.');
+            });
+
+            navigator.serviceWorker.onmessage = function(messageevent) {
+                console.log(messageevent);
+            }
+        });
+
+        // bindEvent(window, 'beforeunload', () => {
+        //     if(notSynced.length > 0) localStorage.setItem(localKey, JSON.stringify(notSynced));
+        // });
+    }
+    else console.log('Log: Service worker not supported.')
+})();
---
+++ media-net/service-worker.js
@@ -0,0 +1,83 @@
+var version = 'v1',
+fixUrl = (purl) => {
+	var // purl = new URL(url),
+		queries = purl.search.replace(/^\?/, '').split('&'),
+		queryArr = [],
+		switchObject = {
+			interaction: "event",
+			client: "customer",
+			os_name: "operating_system_name", 
+			x1: "utm_source",
+			x2: "utm_medium",
+			x3: "utm_campaign",
+			landing_url: "campaign_url"
+		},
+		split;
+
+	queries.forEach((el) => {
+		split = el.split('=');
+
+		if(switchObject[split[0]] !== undefined) queryArr.push(`${switchObject[split[0]]}=${split[1]}`);
+		else queryArr.push(`${split[0]}=${split[1]}`);
+	});
+
+	return `${purl.protocol}//${purl.host}${purl.pathname}?${queryArr.join('&')}${purl.hash}`;
+};
+
+self.addEventListener('install', (event) => {
+    console.log('WORKER: install event in progress.');
+    event.waitUntil(
+        caches
+        .open(`${version}:pixel`)
+        .then((cache) => {
+            return cache.addAll([
+				'/media-net/', 
+				'/media-net/image/', 
+				'/media-net/image/pixel.gif'])
+        })
+        .then(function() {
+            console.log('WORKER: install completed');
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    const currentCaches = [`${version}:pixel`];
+    event.waitUntil(
+		caches.keys().then(cacheNames => {
+			return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+		}).then(cachesToDelete => {
+			return Promise.all(cachesToDelete.map(cacheToDelete => {
+				return caches.delete(cacheToDelete);
+			}));
+		}).then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+	console.log('WORKER: fetch event in progress.');
+	event.respondWith(async function(){
+		var url = new URL(event.request.url);
+		console.log(url);
+		if(/pixel.gif$/.test(url.pathname)) {
+			// if(navigator.onLine === false){
+			// 	caches.open(`${version}:pixel`)
+			// 	.then(cache =>
+			// 		caches.match(event.request).then((result) => {
+
+			// 			return result;
+			// 		})
+			// 	)
+			// }
+
+			return fetch(parseUrl(url))
+			.then((response) => {
+				return response;
+			})
+			.catch((err) => {
+				console.log(err);
+			})
+		}
+		else return fetch(event.request);
+	}());
+})
