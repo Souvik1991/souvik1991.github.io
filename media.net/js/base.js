68217d65fedfef0456036f99f163e0b5b8fa584a
Souvik Maity <souvik.maity200@gmail.com>
2020-05-18 17:39:42 +0530

    local commit

--- media.net/js/base.js
+++ media.net/js/base.js
@@ -17,14 +17,14 @@
             console.log(images)
         })
 
-        // function reqListener () {
-        //     console.log(this.responseText);
-        // }
+        function reqListener () {
+            console.log(this.responseText);
+        }
         
-        // var oReq = new XMLHttpRequest();
-        // oReq.addEventListener("load", reqListener);
-        // oReq.open("GET", `${window.location.protocol}//${window.location.host}/media.net/image/pixel.gif?interaction=UserClick&client=ad_media&os_name=macos&x1=google&x2=email&x3=pdfconvert&landing_url=abcd1`);
-        // oReq.send();
+        var oReq = new XMLHttpRequest();
+        oReq.addEventListener("load", reqListener);
+        oReq.open("GET", `${window.location.protocol}//${window.location.host}/media.net/image/pixel.gif?interaction=UserClick&client=ad_media&os_name=macos&x1=google&x2=email&x3=pdfconvert&landing_url=abcd1`);
+        oReq.send();
 
         var image = document.createElement('img');
         image.setAttribute('src', `${window.location.protocol}//${window.location.host}/media.net/image/pixel.gif?interaction=UserClick&client=ad_media&os_name=macos&x1=google&x2=email&x3=pdfconvert&landing_url=abcd1`);
