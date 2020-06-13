c04126c0dc6132711f378a34fc3b04667dbfc9b0
Souvik Maity <souvik.maity91@gmail.com>
2020-06-13 17:01:33 +0530

    Create precache-manifest.5e3bca5e4d2156c8d3afc191ef88ddfa.js

---
+++ tic-tac-toe/precache-manifest.5e3bca5e4d2156c8d3afc191ef88ddfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09e36ff754efed55e402397cfdde789e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e5e0643202c69b1e2b7",
+    "url": "/static/css/main.a6433f43.chunk.css"
+  },
+  {
+    "revision": "7bb79f98e556a9405dbd",
+    "url": "/static/js/2.13e9ab5e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.13e9ab5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e5e0643202c69b1e2b7",
+    "url": "/static/js/main.7f38b929.chunk.js"
+  },
+  {
+    "revision": "bcb4c4c8973f259d5185",
+    "url": "/static/js/runtime-main.13d7ae64.js"
+  }
+]);
